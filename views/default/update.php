2a616de397117774a4bb875a539ac397026c53d8
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-08-21 14:55:14 +0100

    updated to work with the run_in_a_subdirectory branch

--- views/default/update.php
+++ views/default/update.php
@@ -1,7 +1,7 @@
 <?php 	$this->breadcrumbs=array($this->module->id);
 	$this->header();
 ?>
-<h3 class="withEventIcon" style="background:transparent url(<?php echo $this->imgPath?>medium.png) center left no-repeat;"><?php echo $this->event_type->name ?></h3>
+<h3 class="withEventIcon" style="background:transparent url(<?php echo $this->assetPath?>/img/medium.png) center left no-repeat;"><?php echo $this->event_type->name ?></h3>
 
 <div>
 	<?php 		$form = $this->beginWidget('BaseEventTypeCActiveForm', array(
@@ -17,7 +17,7 @@
 	<?php  $this->displayErrors($errors)?>
 		<div class="cleartall"></div>
 		<div class="form_button">
-			<img class="loader" style="display: none;" src="/img/ajax-loader.gif" alt="loading..." />&nbsp;
+			<img class="loader" style="display: none;" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." />&nbsp;
 			<button type="submit" class="classy green venti" id="et_save" name="save"><span class="button-span button-span-green">Save</span></button>
 			<button type="submit" class="classy red venti" id="et_cancel" name="cancel"><span class="button-span button-span-red">Cancel</span></button>
 		</div>
