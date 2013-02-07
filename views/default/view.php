7c66271f68f5b0f89b90f00c90d97339e28f7a30
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-02-07 14:37:27 +0000

    Update asset locations

--- views/default/view.php
+++ views/default/view.php
@@ -1,7 +1,7 @@
 <?php 	$this->breadcrumbs=array($this->module->id);
 	$this->header();
 ?>
-<h3 class="withEventIcon" style="background:transparent url(<?php echo $this->imgPath?>medium.png) center left no-repeat;"><?php  echo $this->event_type->name ?></h3>
+<h3 class="withEventIcon" style="background:transparent url(<?php echo $this->assetPath?>/img/medium.png) center left no-repeat;"><?php  echo $this->event_type->name ?></h3>
 
 <div>
 	<?php  $this->renderDefaultElements($this->action->id); ?>	<?php  $this->renderOptionalElements($this->action->id); ?>
