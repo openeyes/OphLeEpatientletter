ca9b9660bdcc800b467a278ed3c3cc52a880c28d
Mark <m@rkw.me>
2013-12-12 10:12:02 +0000

    [OE-3856] consolidated migrations

--- migrations/m131204_165643_table_versioning.php
+++ migrations/m131204_165643_table_versioning.php
@@ -6,30 +6,30 @@ class m131204_165643_table_versioning extends CDbMigration
 	{
 		$this->execute("
 CREATE TABLE `et_ophleepatientletter_epatientletter_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `epatient_id` int(10) NOT NULL,
-  `epatient_printed` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `epatient_cc_gp` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `epatient_hosnum` varchar(255) COLLATE utf8_bin DEFAULT '',
-  `importinfo` varchar(255) COLLATE utf8_bin DEFAULT '',
-  `patient_id` int(10) unsigned DEFAULT NULL,
-  `letter_html` text COLLATE utf8_bin,
-  `recipient_html` text COLLATE utf8_bin,
-  `date_html` text COLLATE utf8_bin,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `epatient_id` (`epatient_id`),
-  KEY `acv_et_ophleepatientletter_epatientletter_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_et_ophleepatientletter_epatientletter_cui_fk` (`created_user_id`),
-  KEY `acv_et_ophleepatientletter_epatientletter_ev_fk` (`event_id`),
-  KEY `acv_idx_et_ophleepatientletter_epatientletter_epatient_hosnum` (`epatient_hosnum`),
-  CONSTRAINT `acv_et_ophleepatientletter_epatientletter_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophleepatientletter_epatientletter_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophleepatientletter_epatientletter_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`epatient_id` int(10) NOT NULL,
+	`epatient_printed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+	`epatient_cc_gp` tinyint(1) unsigned NOT NULL DEFAULT '0',
+	`epatient_hosnum` varchar(255) COLLATE utf8_bin DEFAULT '',
+	`importinfo` varchar(255) COLLATE utf8_bin DEFAULT '',
+	`patient_id` int(10) unsigned DEFAULT NULL,
+	`letter_html` text COLLATE utf8_bin,
+	`recipient_html` text COLLATE utf8_bin,
+	`date_html` text COLLATE utf8_bin,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `epatient_id` (`epatient_id`),
+	KEY `acv_et_ophleepatientletter_epatientletter_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_et_ophleepatientletter_epatientletter_cui_fk` (`created_user_id`),
+	KEY `acv_et_ophleepatientletter_epatientletter_ev_fk` (`event_id`),
+	KEY `acv_idx_et_ophleepatientletter_epatientletter_epatient_hosnum` (`epatient_hosnum`),
+	CONSTRAINT `acv_et_ophleepatientletter_epatientletter_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophleepatientletter_epatientletter_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophleepatientletter_epatientletter_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -44,10 +44,16 @@ CREATE TABLE `et_ophleepatientletter_epatientletter_version` (
 		$this->addColumn('et_ophleepatientletter_epatientletter_version','version_id','int(10) unsigned NOT NULL');
 		$this->addPrimaryKey('version_id','et_ophleepatientletter_epatientletter_version','version_id');
 		$this->alterColumn('et_ophleepatientletter_epatientletter_version','version_id','int(10) unsigned NOT NULL AUTO_INCREMENT');
+
+		$this->addColumn('et_ophleepatientletter_epatientletter','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophleepatientletter_epatientletter_version','deleted','tinyint(1) unsigned not null');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('et_ophleepatientletter_epatientletter','deleted');
+		$this->dropColumn('et_ophleepatientletter_epatientletter_version','deleted');
+
 		$this->dropTable('et_ophleepatientletter_epatientletter_version');
 	}
 }
